8123014c2d94c9fc33f7668883ee6e8298481baf
Oleg Knyazev <climberdigital@gmail.com>
2025-06-28 20:10:31 +0300

    Add setup bash script

---
+++ server/setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ -d "local" ]; then
+  echo "Error: Directory 'local' already exists."
+  exit 1
+fi
+
+# Example using a here-document to execute a block of commands
+bash << EOF
+echo "Copying defaults into a gitignored local directory..."
+cp -r ./default ./local
+mv ./local/aliases-template.sh ./local/aliases.sh && mv ./local/docker-compose-template.yml ./docker-compose.yml
+echo "Cleaning up redundant files..."
+rm ./local/nginx/virtual-host-https-template.conf && rm ./local/nginx/virtual-host-template.conf
+echo "Setup complete!"
+EOF
